cd8746578759022cc912bbf10a856a7ac8eb507b
samlyme <sam@samly.me>
2026-01-29 17:28:39 -0800

    ch1

---
+++ CS4080/hw1/truthyness.js
@@ -0,0 +1,8 @@
+const truthy = []
+const falsy = null;
+
+if (truthy) console.log("[] is true!");
+if (!falsy) console.log("null is false!");
+
+// [] is true!
+// null is false!
---
+++ CS4080/hw1/truthyness.py
@@ -0,0 +1,11 @@
+empty_arr = []
+arr = [1,2,3]
+
+if not empty_arr:
+    print("empty_arr is false!")
+
+if arr:
+    print("arr is true!")
+
+# empty_arr is true!
+# arr is true!
