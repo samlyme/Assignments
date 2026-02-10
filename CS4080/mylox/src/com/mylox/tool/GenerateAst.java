475f6ea34ef2b7559e99f482510d629955d3e376
samlyme <sam@samly.me>
2026-02-09 21:55:22 -0800

    add vistor pattern to metaprogramming

--- CS4080/mylox/src/com/mylox/tool/GenerateAst.java
+++ CS4080/mylox/src/com/mylox/tool/GenerateAst.java
@@ -36,11 +36,18 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        // TODO: instead of passing around the writer object, store as a field.
+        defineVisitor(writer, baseName, types);
+
         for (String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base accept() method.
+        writer.println();
+        writer.println(" abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -62,6 +69,14 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
         // Fields.
         writer.println();
         for (String field : fields) {
@@ -69,5 +84,18 @@ public class GenerateAst {
         }
 
         writer.println("    }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
+
     }
 }
