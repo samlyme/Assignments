56d8660edb5d8f7f3f68cbc1c4b53c17d621e047
samlyme <sam@samly.me>
2026-02-13 17:54:52 -0800

    mat4170 hw2 ex 9.4

--- MAT4170/HW2/count_orders.py
+++ MAT4170/HW2/count_orders.py
@@ -1,5 +1,5 @@
 
-from math import comb, factorial, lcm
+from math import comb, factorial, lcm, prod
 
 
 def partitions(n: int):
@@ -31,3 +31,26 @@ print(p_8_filterd) # [(3, 5)]
 
 ways = comb(8, 3) * factorial(2) * factorial(4)
 print(ways) # 2688
+
+p_19 = partitions(19) # a lot of stuff
+p_19_filterd = filter_for_dups(p_19)
+p_19_filterd = filter_for_lcm(p_19_filterd, 210)
+print(p_19_filterd) # [(2, 2, 3, 5, 7), (1, 1, 2, 3, 5, 7), (1, 5, 6, 7)]
+
+def multinomial_coeff(boxes):
+    n = sum(boxes)
+
+    dups = 1
+    seen = set()
+    for i in boxes:
+        if i in seen:
+            dups += 1
+        seen.add(i)
+
+    return factorial(n) / (prod(factorial(n_i) for n_i in boxes) * factorial(dups))
+
+ways = 0
+for boxes in p_19_filterd:
+    ways += multinomial_coeff(boxes) * prod(factorial(n_i-1) for n_i in boxes)
+# print(factorial(19)) # for debugging
+print(int(ways)) # 1013709170073600
