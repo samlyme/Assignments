687ec7bf8de4fccb86216eba1a6abef69b0054f6
samlyme <sam@samly.me>
2026-02-14 16:31:38 -0800

    format

--- CS4080/mylox/src/com/mylox/lox/Parser.java
+++ CS4080/mylox/src/com/mylox/lox/Parser.java
@@ -35,7 +35,8 @@ primary        → NUMBER | STRING | "true" | "false" | "nil"
  */
 
 class Parser {
-    private static class ParseError extends RuntimeException {}
+    private static class ParseError extends RuntimeException {
+    }
 
     private final List<Token> tokens;
     private int current = 0;
@@ -56,9 +57,9 @@ class Parser {
 
     private Expr expression() {
         return comma();
-   }
+    }
 
-   private Expr comma() {
+    private Expr comma() {
         Expr expr = equality();
 
         while (match(COMMA)) {
@@ -67,21 +68,21 @@ class Parser {
             expr = new Expr.Binary(expr, op, right);
         }
         return expr;
-   }
+    }
 
-   private Expr equality() {
+    private Expr equality() {
         Expr expr = comparison();
 
-       while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-           Token operator = previous();
-           Expr right = comparison();
-           expr = new Expr.Binary(expr, operator, right);
-       }
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
 
-       return expr;
-   }
+        return expr;
+    }
 
-   private Expr comparison() {
+    private Expr comparison() {
         Expr expr = term();
 
         while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
@@ -91,9 +92,9 @@ class Parser {
         }
 
         return expr;
-   }
+    }
 
-   private Expr term() {
+    private Expr term() {
         Expr expr = factor();
 
         while (match(MINUS, PLUS)) {
@@ -103,9 +104,9 @@ class Parser {
         }
 
         return expr;
-   }
+    }
 
-   private Expr factor() {
+    private Expr factor() {
         Expr expr = unary();
 
         while (match(STAR, SLASH)) {
@@ -115,32 +116,32 @@ class Parser {
         }
 
         return expr;
-   }
+    }
 
-   private Expr unary() {
+    private Expr unary() {
         if (match(BANG, MINUS)) return new Expr.Unary(previous(), unary());
         return primary();
-   }
+    }
 
-   private Expr primary() {
-       if (match(FALSE)) return new Expr.Literal(false);
-       if (match(TRUE)) return new Expr.Literal(true);
-       if (match(NIL)) return new Expr.Literal(null);
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
 
-       if (match(NUMBER, STRING)) {
+        if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
-       }
+        }
 
-       if (match(LEFT_PAREN)) {
-           Expr expr = expression();
-           consume(RIGHT_PAREN, "Expect ')' after expression.");
-           return new Expr.Grouping(expr);
-       }
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
 
-       throw error(peek(), "Expect expression.");
-   }
+        throw error(peek(), "Expect expression.");
+    }
 
-   private boolean match(TokenType... types) {
+    private boolean match(TokenType... types) {
         for (TokenType type : types) {
             if (check(type)) {
                 advance();
@@ -148,14 +149,14 @@ class Parser {
             }
         }
         return false;
-   }
+    }
 
-   private Token advance() {
+    private Token advance() {
         // I disagree with the way they implemented in the book!
 //       if (!isAtEnd()) current++;
 //       return previous()
-       if (isAtEnd()) return peek();
-       return tokens.get(current++);
+        if (isAtEnd()) return peek();
+        return tokens.get(current++);
     }
 
     private Token consume(TokenType type, String message) {
@@ -192,22 +193,21 @@ class Parser {
     }
 
 
-
     private boolean check(TokenType type) {
         if (isAtEnd()) return false;
 
         return type == peek().type;
-   }
+    }
 
-   private boolean isAtEnd() {
+    private boolean isAtEnd() {
         return peek().type == EOF;
-   }
+    }
 
-   private Token peek() {
+    private Token peek() {
         return tokens.get(current);
-   }
+    }
 
-   private Token previous() {
-        return tokens.get(current-1); // could throw exception!
-   }
+    private Token previous() {
+        return tokens.get(current - 1); // could throw exception!
+    }
 }
