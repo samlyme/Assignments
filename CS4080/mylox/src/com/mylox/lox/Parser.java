c2768ffaa47d3dcd62bb69dafe06367a15b96cd5
samlyme <sam@samly.me>
2026-02-24 22:58:19 -0800

    working block scope

--- CS4080/mylox/src/com/mylox/lox/Parser.java
+++ CS4080/mylox/src/com/mylox/lox/Parser.java
@@ -36,15 +36,14 @@ primary        → NUMBER | STRING | IDENTIFIER | "true" | "false" | "nil"
                | "(" expression ")" ;
 
 ~~~ CHAPTER 8 ~~~
-Now, we must add functionality for parsing statements, that
-mutate state.
-
 program        → declaration* EOF ;
 declaration    → varDecl
                | statement ;
 varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
 statement      → exprStmt
-               | printStmt ;
+               | printStmt
+               | block ;
+block          → "{" declaration* "}" ;
 exprStmt       → expression ";" ;
 printStmt      → "print" expression ";" ;
 
@@ -80,6 +79,7 @@ class Parser {
 
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
     }
@@ -106,6 +106,16 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr expression() {
