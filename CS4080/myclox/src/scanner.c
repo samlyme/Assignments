8b23df0127697d2622dd5abbd63787af2ed0acd9
samlyme <sam@samly.me>
2026-03-27 01:50:39 -0700

    scanner

--- CS4080/myclox/src/scanner.c
+++ CS4080/myclox/src/scanner.c
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <stdio.h>
 #include <string.h>
 
@@ -21,6 +22,12 @@ void initScanner(const char* source) {
 static bool isDigit(char c) {
     // idk why we don't use std implementation
     return c >= '0' && c <= '9';
+}
+
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || 
+           (c >= 'A' && c <= 'Z') || 
+            c == '_';
 }
 
 static bool isAtEnd() {
@@ -92,12 +99,63 @@ static void skipWhiteSpace() {
     }
 }
 
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
+    if (
+        scanner.current - scanner.start == start + length 
+        && memcmp(scanner.start + start, rest, length) == 0
+    ) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+    switch (*scanner.start) {
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    }
+    return TOKEN_IDENTIFIER; // is here for later, some tokens are special.
+}
+
 static Token number() {
     while (isDigit(peek()) && !isAtEnd()) advance();
     if (peek() == '.' && isDigit(peekNext())) {
         while (isDigit(peek()) && !isAtEnd()) advance();
     }
     return makeToken(TOKEN_NUMBER);
+}
+
+static Token identifier() {
+    while (isAlpha(peek()) && !isAtEnd() || isDigit(peek())) advance();
+
+    return makeToken(identifierType());
 }
 
 static Token string() {
@@ -123,6 +181,8 @@ Token scanToken() {
     char c = advance();
     
     if (isDigit(c)) return number(); // can't do a bigass switch case lmfao
+    if (isAlpha(c)) return identifier();
+
     switch (c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
         case ')': return makeToken(TOKEN_RIGHT_PAREN);
