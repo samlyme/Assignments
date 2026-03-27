0c8db3a06304094187f1c6b30f0bad34a27d06f1
samlyme <sam@samly.me>
2026-03-27 00:33:14 -0700

    basic pipeline

---
+++ CS4080/myclox/src/compiler.h
@@ -0,0 +1,8 @@
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+// I dont understand this function signature in the slightest.
+// why is it void? where does the compiled code go? what?
+void compile(const char* source);
+
+#endif
