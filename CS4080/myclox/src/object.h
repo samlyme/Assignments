6ec74506c85ebc58521f351435bafc42fef2f2fa
samlyme <sam@samly.me>
2026-04-10 16:21:26 -0700

    string equality operation

--- CS4080/myclox/src/object.h
+++ CS4080/myclox/src/object.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "value.h"
 
-#define OBJ_TYPE(value) (AS_OBJ(value)->type))
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
 #define IS_STRING(value)    isObjType(value, OBJ_STRING)
 
@@ -26,6 +26,8 @@ struct ObjString {
 };
 
 ObjString* copyString(const char* chars, int length);
+
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
     return IS_OBJ(value) && AS_OBJ(value)->type == type;
