315b3f68839c2a91b0506277cb084b2799bd13d9
samlyme <sam@samly.me>
2026-04-17 16:41:24 -0700

    dynamic allocation for table

--- CS4080/myclox/src/table.c
+++ CS4080/myclox/src/table.c
@@ -2,9 +2,11 @@
 #include <string.h>
 
 #include "memory.h"
-#include "object.h"
+#include "object.h" // IWYU pragma: keepignore
 #include "table.h"
 #include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
 
 void initTable(Table* table) {
     table->count = 0;
@@ -15,4 +17,57 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
+}
+
+Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+
+    for (;;) {
+        // don't worry, the edge case here will be fixed by a tombstone.
+        Entry* entry = &entries[index];
+        if (entry->key == key || entry->key == NULL) {
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+    Entry* entries = ALLOCATE(Entry, capacity);
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+
+        Entry* dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey) {
+        table->count++;
+    }
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
 }
--- CS4080/myclox/src/table.h
+++ CS4080/myclox/src/table.h
@@ -17,5 +17,6 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTalbe(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
 
 #endif
