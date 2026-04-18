9b4226ee6094e3a82565063c1c23dc5e5744905a
samlyme <sam@samly.me>
2026-04-17 17:16:43 -0700

    done

--- CS4080/myclox/src/vm.c
+++ CS4080/myclox/src/vm.c
@@ -7,6 +7,7 @@
 #include "debug.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 
 VM vm; 
@@ -31,9 +32,11 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
