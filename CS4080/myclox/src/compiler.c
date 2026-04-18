c16d905be176e3e886aafc48b2f1b9fe0eedb2dd
samlyme <sam@samly.me>
2026-04-18 11:49:33 -0700

    error synchronize

--- CS4080/myclox/src/compiler.c
+++ CS4080/myclox/src/compiler.c
@@ -277,6 +277,30 @@ static ParseRule* getRule(TokenType type) {
     return &rules[type];
 }
 
+static void synchronize() {
+  parser.panicMode = false;
+
+  while (parser.current.type != TOKEN_EOF) {
+    if (parser.previous.type == TOKEN_SEMICOLON) return;
+    switch (parser.current.type) {
+      case TOKEN_CLASS:
+      case TOKEN_FUN:
+      case TOKEN_VAR:
+      case TOKEN_FOR:
+      case TOKEN_IF:
+      case TOKEN_WHILE:
+      case TOKEN_PRINT:
+      case TOKEN_RETURN:
+        return;
+
+      default:
+        ; // Do nothing.
+    }
+
+    advance();
+  }
+}
+
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
@@ -295,6 +319,7 @@ static void expressionStatement() {
 
 static void declaration() {
     statement();
+    if (parser.panicMode) synchronize();
 }
 
 static void statement() {
