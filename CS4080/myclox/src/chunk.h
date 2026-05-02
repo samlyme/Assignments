211fe0bd2a7db4a8b14c0fc9085b245e38242155
samlyme <sam@samly.me>
2026-05-02 15:33:56 -0700

    14.3.1

---
+++ CS4080/myclox/src/chunk.h
@@ -0,0 +1,19 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef enum {
+  OP_RETURN,
+} OpCode;
+
+typedef struct {
+  // some magic to make this feel like a dynamic array "object".
+  int count;
+  int capacity;
+  uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+
+#endif
---
+++ CS4080/myclox/src/main.c
@@ -0,0 +1,5 @@
+#include "common.h" // IWYU pragma: keep
+
+int main(int argc, const char* argv[]) {
+  return 0;
+}
