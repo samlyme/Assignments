c5364fecba6771bcbd8136b0442fe7a254167d6d
samlyme <sam@samly.me>
2026-05-02 18:43:45 -0700

    done

--- CS4080/myclox/src/vm.c
+++ CS4080/myclox/src/vm.c
@@ -24,6 +24,12 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+  do {                                                                         \
+    double b = pop();                                                          \
+    double a = pop();                                                          \
+    push(a op b);                                                              \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -43,10 +49,12 @@ static InterpretResult run() {
         push(constant);
         break;
       }
-      case OP_NEGATE: {
-        push(-pop());
-        break;
-      }
+      case OP_ADD: BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE: BINARY_OP(/); break;
+
+      case OP_NEGATE: push(-pop()); break;
       case OP_RETURN: {
         // the last value on the stack is the returned value.
         printValue(pop());
@@ -56,6 +64,7 @@ static InterpretResult run() {
     }
   }
 
+#undef BINARY_OP
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
