c65da96a964b64b52499164224a06dd62deea60e
samlyme <sam@samly.me>
2026-05-03 14:39:08 -0700

    feat: nil, true, false

--- CS4080/myclox/src/debug.c
+++ CS4080/myclox/src/debug.c
@@ -37,6 +37,9 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
     case OP_CONSTANT: return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_NIL: return simpleInstruction("OP_NIL", offset);
+    case OP_TRUE: return simpleInstruction("OP_TRUE", offset);
+    case OP_FALSE: return simpleInstruction("OP_FALSE", offset);
     case OP_ADD: return simpleInstruction("OP_ADD", offset);
     case OP_SUBTRACT: return simpleInstruction("OP_SUBTRACT", offset);
     case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset);
