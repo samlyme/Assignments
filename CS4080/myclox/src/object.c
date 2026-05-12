9a0c81cd40915b6c1e1c1b9915a33ec33427ca05
samlyme <sam@samly.me>
2026-05-12 14:51:25 -0700

    done

--- CS4080/myclox/src/object.c
+++ CS4080/myclox/src/object.c
@@ -1,90 +1,135 @@
+//> Strings object-c
 #include <stdio.h>
 #include <string.h>
 
 #include "memory.h"
 #include "object.h"
+//> Hash Tables object-include-table
 #include "table.h"
+//< Hash Tables object-include-table
 #include "value.h"
 #include "vm.h"
+//> allocate-obj
 
-#define ALLOCATE_OBJ(type, objectType) \
-    (type*)allocateObject(sizeof(type), objectType)
+#define ALLOCATE_OBJ(type, objectType)                                         \
+  (type *)allocateObject(sizeof(type), objectType)
+//< allocate-obj
+//> allocate-object
 
-static Obj* allocateObject(size_t size, ObjType type) {
-  Obj* object = (Obj*)reallocate(NULL, 0, size);
+static Obj *allocateObject(size_t size, ObjType type) {
+  Obj *object = (Obj *)reallocate(NULL, 0, size);
   object->type = type;
+  //> Garbage Collection init-is-marked
   object->isMarked = false;
+  //< Garbage Collection init-is-marked
+  //> add-to-list
 
   object->next = vm.objects;
   vm.objects = object;
+  //< add-to-list
+  //> Garbage Collection debug-log-allocate
 
 #ifdef DEBUG_LOG_GC
-  printf("%p allocate %zu for %d\n", (void*)object, size, type);
+  printf("%p allocate %zu for %d\n", (void *)object, size, type);
 #endif
 
+  //< Garbage Collection debug-log-allocate
   return object;
 }
-ObjBoundMethod* newBoundMethod(Value receiver,
-                               ObjClosure* method) {
-  ObjBoundMethod* bound = ALLOCATE_OBJ(ObjBoundMethod,
-                                       OBJ_BOUND_METHOD);
+//< allocate-object
+//> Methods and Initializers new-bound-method
+ObjBoundMethod *newBoundMethod(Value receiver, ObjClosure *method) {
+  ObjBoundMethod *bound = ALLOCATE_OBJ(ObjBoundMethod, OBJ_BOUND_METHOD);
   bound->receiver = receiver;
   bound->method = method;
   return bound;
 }
-ObjClass* newClass(ObjString* name) {
-  ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+//< Methods and Initializers new-bound-method
+//> Classes and Instances new-class
+ObjClass *newClass(ObjString *name) {
+  ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
   klass->name = name; // [klass]
+                      //> Methods and Initializers init-methods
   initTable(&klass->methods);
+  //< Methods and Initializers init-methods
   return klass;
 }
-ObjClosure* newClosure(ObjFunction* function) {
-  ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
-                                   function->upvalueCount);
+//< Classes and Instances new-class
+//> Closures new-closure
+ObjClosure *newClosure(ObjFunction *function) {
+  //> allocate-upvalue-array
+  ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue *, function->upvalueCount);
   for (int i = 0; i < function->upvalueCount; i++) {
     upvalues[i] = NULL;
   }
 
-  ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+  //< allocate-upvalue-array
+  ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
   closure->function = function;
+  //> init-upvalue-fields
   closure->upvalues = upvalues;
   closure->upvalueCount = function->upvalueCount;
+  //< init-upvalue-fields
   return closure;
 }
-ObjFunction* newFunction() {
-  ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+//< Closures new-closure
+//> Calls and Functions new-function
+ObjFunction *newFunction() {
+  ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
+  //> Closures init-upvalue-count
   function->upvalueCount = 0;
+  //< Closures init-upvalue-count
   function->name = NULL;
   initChunk(&function->chunk);
   return function;
 }
-ObjInstance* newInstance(ObjClass* klass) {
-  ObjInstance* instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
+//< Calls and Functions new-function
+//> Classes and Instances new-instance
+ObjInstance *newInstance(ObjClass *klass) {
+  ObjInstance *instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
   instance->klass = klass;
   initTable(&instance->fields);
   return instance;
 }
-ObjNative* newNative(NativeFn function) {
-  ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+//< Classes and Instances new-instance
+//> Calls and Functions new-native
+ObjNative *newNative(NativeFn function) {
+  ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
   native->function = function;
   return native;
 }
+//< Calls and Functions new-native
 
-static ObjString* allocateString(char* chars, int length,
-                                 uint32_t hash) {
-  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+/* Strings allocate-string < Hash Tables allocate-string
+static ObjString* allocateString(char* chars, int length) {
+*/
+//> allocate-string
+//> Hash Tables allocate-string
+static ObjString *allocateString(char *chars, int length, uint32_t hash) {
+  //< Hash Tables allocate-string
+  ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
   string->length = length;
   string->chars = chars;
+  //> Hash Tables allocate-store-hash
   string->hash = hash;
+  //< Hash Tables allocate-store-hash
+  //> Hash Tables allocate-store-string
+  //> Garbage Collection push-string
 
   push(OBJ_VAL(string));
+  //< Garbage Collection push-string
   tableSet(&vm.strings, string, NIL_VAL);
+  //> Garbage Collection pop-string
   pop();
 
+  //< Garbage Collection pop-string
+  //< Hash Tables allocate-store-string
   return string;
 }
-static uint32_t hashString(const char* key, int length) {
+//< allocate-string
+//> Hash Tables hash-string
+static uint32_t hashString(const char *key, int length) {
   uint32_t hash = 2166136261u;
   for (int i = 0; i < length; i++) {
     hash ^= (uint8_t)key[i];
@@ -92,68 +137,183 @@ static uint32_t hashString(const char* key, int length) {
   }
   return hash;
 }
-ObjString* takeString(char* chars, int length) {
+//< Hash Tables hash-string
+//> take-string
+ObjString *takeString(char *chars, int length) {
+  /* Strings take-string < Hash Tables take-string-hash
+    return allocateString(chars, length);
+  */
+  //> Hash Tables take-string-hash
   uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindString(&vm.strings, chars, length,
-                                        hash);
+  //> take-string-intern
+  ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
   if (interned != NULL) {
     FREE_ARRAY(char, chars, length + 1);
     return interned;
   }
 
+  //< take-string-intern
   return allocateString(chars, length, hash);
+  //< Hash Tables take-string-hash
 }
-ObjString* copyString(const char* chars, int length) {
+//< take-string
+ObjString *copyString(const char *chars, int length) {
+  //> Hash Tables copy-string-hash
   uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindString(&vm.strings, chars, length,
-                                        hash);
-  if (interned != NULL) return interned;
+  //> copy-string-intern
+  ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
+  if (interned != NULL)
+    return interned;
 
-  char* heapChars = ALLOCATE(char, length + 1);
+  //< copy-string-intern
+  //< Hash Tables copy-string-hash
+  char *heapChars = ALLOCATE(char, length + 1);
   memcpy(heapChars, chars, length);
   heapChars[length] = '\0';
+  /* Strings object-c < Hash Tables copy-string-allocate
+    return allocateString(heapChars, length);
+  */
+  //> Hash Tables copy-string-allocate
   return allocateString(heapChars, length, hash);
+  //< Hash Tables copy-string-allocate
 }
-ObjUpvalue* newUpvalue(Value* slot) {
-  ObjUpvalue* upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+
+Value takeStringValue(char *chars, int length) {
+  if (length <= SHORT_STRING_MAX) {
+    Value value = SHORT_STRING_VAL(chars, length);
+    FREE_ARRAY(char, chars, length + 1);
+    return value;
+  }
+
+  return OBJ_VAL(takeString(chars, length));
+}
+
+Value copyStringValue(const char *chars, int length) {
+  if (length <= SHORT_STRING_MAX) {
+    return SHORT_STRING_VAL(chars, length);
+  }
+
+  return OBJ_VAL(copyString(chars, length));
+}
+
+int stringValueLength(Value value) {
+  if (IS_SHORT_STRING(value))
+    return shortStringLength(value);
+  return AS_STRING(value)->length;
+}
+
+void copyStringValueChars(Value value, char *chars) {
+  if (IS_SHORT_STRING(value)) {
+    copyShortString(value, chars);
+    return;
+  }
+
+  ObjString *string = AS_STRING(value);
+  memcpy(chars, string->chars, string->length);
+}
+
+bool stringValuesEqual(Value a, Value b) {
+  int length = stringValueLength(a);
+  if (length != stringValueLength(b))
+    return false;
+
+  if (!IS_SHORT_STRING(a) && !IS_SHORT_STRING(b)) {
+    return AS_STRING(a) == AS_STRING(b);
+  }
+
+  if (IS_SHORT_STRING(a)) {
+    char aChars[SHORT_STRING_MAX];
+    copyShortString(a, aChars);
+    if (IS_SHORT_STRING(b)) {
+      char bChars[SHORT_STRING_MAX];
+      copyShortString(b, bChars);
+      return memcmp(aChars, bChars, length) == 0;
+    }
+    return memcmp(aChars, AS_STRING(b)->chars, length) == 0;
+  }
+
+  char chars[SHORT_STRING_MAX];
+  copyShortString(b, chars);
+  return memcmp(AS_STRING(a)->chars, chars, length) == 0;
+}
+
+void printStringValue(Value value) {
+  if (IS_SHORT_STRING(value)) {
+    char chars[SHORT_STRING_MAX];
+    int length = shortStringLength(value);
+    copyShortString(value, chars);
+    printf("%.*s", length, chars);
+    return;
+  }
+
+  ObjString *string = AS_STRING(value);
+  printf("%.*s", string->length, string->chars);
+}
+//> Closures new-upvalue
+ObjUpvalue *newUpvalue(Value *slot) {
+  ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+  //> init-closed
   upvalue->closed = NIL_VAL;
+  //< init-closed
   upvalue->location = slot;
+  //> init-next
   upvalue->next = NULL;
+  //< init-next
   return upvalue;
 }
-static void printFunction(ObjFunction* function) {
+//< Closures new-upvalue
+//> Calls and Functions print-function-helper
+static void printFunction(ObjFunction *function) {
+  //> print-script
   if (function->name == NULL) {
     printf("<script>");
     return;
   }
+  //< print-script
   printf("<fn %s>", function->name->chars);
 }
+//< Calls and Functions print-function-helper
+//> print-object
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
-    case OBJ_BOUND_METHOD:
-      printFunction(AS_BOUND_METHOD(value)->method->function);
-      break;
-    case OBJ_CLASS:
-      printf("%s", AS_CLASS(value)->name->chars);
-      break;
-    case OBJ_CLOSURE:
-      printFunction(AS_CLOSURE(value)->function);
-      break;
-    case OBJ_FUNCTION:
-      printFunction(AS_FUNCTION(value));
-      break;
-    case OBJ_INSTANCE:
-      printf("%s instance",
-             AS_INSTANCE(value)->klass->name->chars);
-      break;
-    case OBJ_NATIVE:
-      printf("<native fn>");
-      break;
-    case OBJ_STRING:
-      printf("%s", AS_CSTRING(value));
-      break;
-    case OBJ_UPVALUE:
-      printf("upvalue");
-      break;
+    //> Methods and Initializers print-bound-method
+  case OBJ_BOUND_METHOD:
+    printFunction(AS_BOUND_METHOD(value)->method->function);
+    break;
+    //< Methods and Initializers print-bound-method
+    //> Classes and Instances print-class
+  case OBJ_CLASS:
+    printf("%s", AS_CLASS(value)->name->chars);
+    break;
+    //< Classes and Instances print-class
+    //> Closures print-closure
+  case OBJ_CLOSURE:
+    printFunction(AS_CLOSURE(value)->function);
+    break;
+    //< Closures print-closure
+    //> Calls and Functions print-function
+  case OBJ_FUNCTION:
+    printFunction(AS_FUNCTION(value));
+    break;
+    //< Calls and Functions print-function
+    //> Classes and Instances print-instance
+  case OBJ_INSTANCE:
+    printf("%s instance", AS_INSTANCE(value)->klass->name->chars);
+    break;
+    //< Classes and Instances print-instance
+    //> Calls and Functions print-native
+  case OBJ_NATIVE:
+    printf("<native fn>");
+    break;
+    //< Calls and Functions print-native
+  case OBJ_STRING:
+    printf("%s", AS_CSTRING(value));
+    break;
+    //> Closures print-upvalue
+  case OBJ_UPVALUE:
+    printf("upvalue");
+    break;
+    //< Closures print-upvalue
   }
 }
+//< print-object
--- CS4080/myclox/src/value.h
+++ CS4080/myclox/src/value.h
@@ -1,51 +1,228 @@
+//> Chunks of Bytecode value-h
 #ifndef clox_value_h
 #define clox_value_h
+//> Optimization include-string
+
+#include <string.h>
+//< Optimization include-string
 
 #include "common.h"
 
+//> Strings forward-declare-obj
 typedef struct Obj Obj;
+//> forward-declare-obj-string
 typedef struct ObjString ObjString;
+//< forward-declare-obj-string
 
+//< Strings forward-declare-obj
+#define SHORT_STRING_MAX 5
+
+//> Optimization nan-boxing
+#ifdef NAN_BOXING
+//> qnan
+
+//> sign-bit
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+//< sign-bit
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+//< qnan
+//> tags
+
+#define TAG_NIL   1 // 01.
+#define TAG_FALSE 2 // 10.
+#define TAG_TRUE  3 // 11.
+#define TAG_SHORT_STRING 4 // 100.
+//< tags
+
+typedef uint64_t Value;
+//> is-number
+
+//> is-bool
+#define IS_BOOL(value)      (((value) | 1) == TRUE_VAL)
+//< is-bool
+//> is-nil
+#define IS_NIL(value)       ((value) == NIL_VAL)
+//< is-nil
+#define IS_NUMBER(value)    (((value) & QNAN) != QNAN)
+//< is-number
+//> is-short-string
+#define IS_SHORT_STRING(value) \
+    (((value) & (SIGN_BIT | QNAN | 0x7)) == (QNAN | TAG_SHORT_STRING))
+//< is-short-string
+//> is-obj
+#define IS_OBJ(value) \
+    (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+//< is-obj
+//> as-number
+
+//> as-bool
+#define AS_BOOL(value)      ((value) == TRUE_VAL)
+//< as-bool
+#define AS_NUMBER(value)    valueToNum(value)
+//< as-number
+//> as-obj
+#define AS_OBJ(value) \
+    ((Obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+//< as-obj
+//> number-val
+
+//> bool-val
+#define BOOL_VAL(b)     ((b) ? TRUE_VAL : FALSE_VAL)
+//< bool-val
+//> false-true-vals
+#define FALSE_VAL       ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL        ((Value)(uint64_t)(QNAN | TAG_TRUE))
+//< false-true-vals
+//> nil-val
+#define NIL_VAL         ((Value)(uint64_t)(QNAN | TAG_NIL))
+//< nil-val
+#define NUMBER_VAL(num) numToValue(num)
+//< number-val
+//> short-string-val
+#define SHORT_STRING_VAL(chars, length) shortStringVal(chars, length)
+//< short-string-val
+//> obj-val
+#define OBJ_VAL(obj) \
+    (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+//< obj-val
+//> value-to-num
+
+static inline double valueToNum(Value value) {
+  double num;
+  memcpy(&num, &value, sizeof(Value));
+  return num;
+}
+//< value-to-num
+//> num-to-value
+
+static inline Value numToValue(double num) {
+  Value value;
+  memcpy(&value, &num, sizeof(double));
+  return value;
+}
+//< num-to-value
+//> short-string-helpers
+
+static inline int shortStringLength(Value value) {
+  return (int)((value >> 3) & 0x7);
+}
+
+static inline Value shortStringVal(const char* chars, int length) {
+  Value value = QNAN | TAG_SHORT_STRING | ((uint64_t)length << 3);
+  for (int i = 0; i < length; i++) {
+    value |= (uint64_t)(uint8_t)chars[i] << (6 + i * 8);
+  }
+  return value;
+}
+
+static inline void copyShortString(Value value, char* chars) {
+  int length = shortStringLength(value);
+  for (int i = 0; i < length; i++) {
+    chars[i] = (char)((value >> (6 + i * 8)) & 0xff);
+  }
+}
+//< short-string-helpers
+
+#else
+
+//< Optimization nan-boxing
+//> Types of Values value-type
 typedef enum {
   VAL_BOOL,
   VAL_NIL, // [user-types]
   VAL_NUMBER,
+  VAL_SHORT_STRING,
+//> Strings val-obj
   VAL_OBJ
+//< Strings val-obj
 } ValueType;
 
+//< Types of Values value-type
+/* Chunks of Bytecode value-h < Types of Values value
+typedef double Value;
+*/
+//> Types of Values value
 typedef struct {
   ValueType type;
   union {
     bool boolean;
     double number;
+    struct {
+      int length;
+      char chars[SHORT_STRING_MAX];
+    } string;
+//> Strings union-object
     Obj* obj;
+//< Strings union-object
   } as; // [as]
 } Value;
+//< Types of Values value
+//> Types of Values is-macros
 
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_SHORT_STRING(value) ((value).type == VAL_SHORT_STRING)
+//> Strings is-obj
 #define IS_OBJ(value)     ((value).type == VAL_OBJ)
+//< Strings is-obj
+//< Types of Values is-macros
+//> Types of Values as-macros
 
+//> Strings as-obj
 #define AS_OBJ(value)     ((value).as.obj)
+//< Strings as-obj
 #define AS_BOOL(value)    ((value).as.boolean)
 #define AS_NUMBER(value)  ((value).as.number)
+//< Types of Values as-macros
+//> Types of Values value-macros
 
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define SHORT_STRING_VAL(chars, length) shortStringVal(chars, length)
+//> Strings obj-val
 #define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+//< Strings obj-val
+//< Types of Values value-macros
+static inline int shortStringLength(Value value) {
+  return value.as.string.length;
+}
+
+static inline Value shortStringVal(const char* chars, int length) {
+  Value value;
+  value.type = VAL_SHORT_STRING;
+  value.as.string.length = length;
+  memcpy(value.as.string.chars, chars, length);
+  return value;
+}
+
+static inline void copyShortString(Value value, char* chars) {
+  memcpy(chars, value.as.string.chars, value.as.string.length);
+}
+//> Optimization end-if-nan-boxing
+
+#endif
+//< Optimization end-if-nan-boxing
+//> value-array
 
 typedef struct {
   int capacity;
   int count;
   Value* values;
 } ValueArray;
+//< value-array
+//> array-fns-h
 
+//> Types of Values values-equal-h
 bool valuesEqual(Value a, Value b);
+//< Types of Values values-equal-h
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
+//< array-fns-h
+//> print-value-h
 void printValue(Value value);
+//< print-value-h
 
 #endif
